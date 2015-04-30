69e9802ab7204b45ba30dabd19b91d1f1358478d
duconstp <duconstp@gmail.com>
2015-04-30 17:45:23 +0200

    tetris attack world 3

--- guiJs/menuConf.js
+++ guiJs/menuConf.js
@@ -85,6 +85,29 @@ w2m9.link = "game.html?game=ta_2_9";
 var w2m10 = new MenuElement("Map 10", world2);
 w2m10.link = "game.html?game=ta_2_10";
 
+
+var world3 = new MenuElement("World 3", tetrisAttack);
+var w3m1 = new MenuElement("Map 1", world3);
+w3m1.link = "game.html?game=ta_3_1";
+var w3m2 = new MenuElement("Map 2", world3);
+w3m2.link = "game.html?game=ta_3_2";
+var w3m3 = new MenuElement("Map 3", world3);
+w3m3.link = "game.html?game=ta_3_3";
+var w3m4 = new MenuElement("Map 4", world3);
+w3m4.link = "game.html?game=ta_3_4";
+var w3m5 = new MenuElement("Map 5", world3);
+w3m5.link = "game.html?game=ta_3_5";
+var w3m6 = new MenuElement("Map 6", world3);
+w3m6.link = "game.html?game=ta_3_6";
+var w3m7 = new MenuElement("Map 7", world3);
+w3m7.link = "game.html?game=ta_3_7";
+var w3m8 = new MenuElement("Map 8", world3);
+w3m8.link = "game.html?game=ta_3_8";
+var w3m9 = new MenuElement("Map 9", world3);
+w3m9.link = "game.html?game=ta_3_9";
+var w3m10 = new MenuElement("Map 10", world3);
+w3m10.link = "game.html?game=ta_3_10";
+
 var multi = new MenuElement("Multi Player", mainMenu);
 var split = new MenuElement("Split Screen", multi);
 split.link = "splitscreen.html?game=classicSplitScreen";
