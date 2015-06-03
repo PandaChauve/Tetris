720a87740bda021b54d87e1ed56ea2d438e2dd99
duconstp <duconstp@gmail.com>
2015-06-03 20:01:45 +0200

    one push at a time

--- js/services/user/storage.js
+++ js/services/user/storage.js
@@ -4,8 +4,30 @@ angular.module('angularApp.factories')
         function UserStorage() {
             this.storage = {};
             this.userId = -1;
+            this.ready = true;
+            this.inSave = false;
+            this.waitingToSave = false;
+            this.eventHandler = [];
         }
 
+        UserStorage.prototype.registerToEvent = function (obj, fct) {
+            this.eventHandler.push({objet: obj, callback: fct});
+        };
+
+        UserStorage.prototype.unregister = function (obj) {
+            for (var i = 0; i < this.eventHandler.length; i += 1) {
+                if (this.eventHandler[i].objet === obj) {
+                    this.eventHandler = this.eventHandler.splice(i, 1);
+                    break;
+                }
+            }
+        };
+
+        UserStorage.prototype.broadcast = function () {
+            for (var i = 0; i < this.eventHandler.length; i += 1) {
+                this.eventHandler[i].callback.apply(this.eventHandler[i].objet);
+            }
+        };
         UserStorage.prototype.load = function load(userid) {
             this.storage = {};
             this.userId = -1;
@@ -48,25 +70,39 @@ angular.module('angularApp.factories')
         };
 
         UserStorage.prototype.set = function (key, value, flush) {
-            if(flush === undefined){
+            if (flush === undefined) {
                 flush = true;
             }
             this.storage[key] = JSON.stringify(value);
             if (this.userId !== -1 && flush) {
+                this.save();
+            }
+        };
+        UserStorage.prototype.save = function () {
+            if(this.inSave){
+                this.waitingToSave = true;
+            }
+            else{
+                this.waitingToSave = false;
+                this.inSave = true;
+                var that = this;
                 $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded"; //FIXME
                 $http.post('http://sylvain.luthana.be/tetrisApi.php?p', {
                     'user_id': this.userId,
                     'type': 'storage',
                     'userdata': JSON.stringify(this.storage)
-                })
-                    .success(function (e) {
-                        console.log(e);
-
-                    })
-                    .error(function (e) {
-                        console.log(e);
-                    });
-
+                }).success(function (e) {
+                    that.inSave = false;
+                    if(that.waitingToSave){
+                        that.save();
+                    }
+                }).error(function (e) {
+                    console.log(e);
+                    this.inSave = false;
+                    if(that.waitingToSave){
+                        that.save();
+                    }
+                });
             }
         };
         return new UserStorage();
