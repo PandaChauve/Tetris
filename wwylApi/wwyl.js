a73c3e6589174c4165f4d235caab4a23f9e3f2a3
duconstp <duconstp@gmail.com>
2015-06-07 12:35:00 +0200

    api source

---
+++ wwylApi/wwyl.js
@@ -0,0 +1,149 @@
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('tetrisApi.sqlite');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var crypto = require('crypto');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    next();
+});
+
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.json({message: 'Sorry this is the api'});
+});
+
+router.route('/scores').post(function (req, res) {
+    db.run("INSERT INTO scores VALUES($score, $user, $map)", {
+        $score: req.body.score,
+        $user: req.body.user,
+        $map: req.body.map
+    });
+
+    res.json({message: 'New score'});
+});
+
+router.route('/scores/:map').get(function (req, res) {
+    db.all("SELECT s.score, u.name FROM scores as s, users as u WHERE s.user_id = u.rowid and s.map=$map ORDER BY score DESC limit 50", {$map: req.params.map}, function (err, rows) {
+        res.json(rows);
+    });
+});
+
+router.route('/users').post(function (req, res) {
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 10; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+
+    var salt = makeid();
+    var shasum = crypto.createHash('sha512');
+    shasum.update(salt + req.body.password);
+    var hash = shasum.digest('hex');
+    db.run("INSERT INTO users VALUES(NULL, $userName, $hash, $salt, NULL)", {
+        $userName: req.body.userName,
+        $hash: hash,
+        $salt: salt
+    }, function (error) {
+        if (error === null) {
+            res.json({
+                success: true,
+                message: "user created"
+            });
+        }
+        else {
+            console.log(error);
+            res.json({
+                success: false,
+                message: "can't create user, already exists ?"
+            });
+
+        }
+    });
+});
+
+router.route('/users/:userid/:hash').put(function (req, res) {
+    db.run("UPDATE users set data=$data where user_id=$userId and hash=$hash", {
+        $hash: req.params.hash,
+        $userId: req.params.userid,
+        $data: req.body.data
+    });
+    res.json({
+        success: true,
+        message: "user updated"
+    });
+}).get(function (req, res) {
+    db.get("SELECT * from users where user_id=$userId and hash=$hash", {
+        $hash: req.params.hash,
+        $userId: req.params.userid
+    }, function (err, row) {
+        if (err || !row) {
+            res.json({
+                success: false, message: "Invalid User"
+            });
+        }
+        else {
+            res.json({
+                id: row.user_id,
+                name: row.name,
+                hash: row.hash,
+                data: row.data,
+                success: true
+            });
+        }
+    });
+});
+
+router.route('/users/validate/:name/:password').get(function (req, res) {
+    db.get("SELECT * FROM users WHERE name = $name", {
+        $name: req.params.name
+    }, function (err, row) {
+        if (err) {
+            res.json({
+                success: false,
+                message: "can't fin user"
+            });
+        }
+        else {
+            var shasum = crypto.createHash('sha512');
+            shasum.update(row.salt + req.params.password);
+            var hash = shasum.digest('hex');
+            if (hash != row.hash) {
+                res.json({
+                    success: false,
+                    message: "invalid password"
+                });
+            }
+            else {
+                res.json({
+                    success: true,
+                    id: row.user_id,
+                    name: row.name,
+                    hash: row.hash,
+                    data: row.data,
+                    message: "user found"
+                });
+            }
+        }
+    });
+});
+
+app.use('/wwylApi', router);
+app.listen(1337);
+
+console.log("wwyl to http://localhost:1337/wwylApi");
+
