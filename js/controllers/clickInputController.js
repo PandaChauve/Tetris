a5e03e5274e5a37e0028651507274b2ac4976213
PandaChauve <duconstp@gmail.com>
2015-07-04 10:09:52 +0200

    removing old manual control

--- js/controllers/clickInputController.js
+++
@@ -1,7 +0,0 @@
-angular.module('angularApp.controllers')
-    .controller('ClickInputCtrl', ['$scope', 'userInput', function ($scope, userInput) {
-        "use strict";
-        $scope.fireKey = function fireKey(key){
-            userInput.press(key);
-        };
-    }]);
