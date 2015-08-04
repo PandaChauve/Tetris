f547b03f2a667802961e793b3bb58e6b30e212e8
PandaChauve <sylvain.chevremont@gmail.com>
2015-08-04 20:35:49 +0200

    new video + moving big resources to separated folder

--- wwylApi/captures/gen.cmd
+++ wwylApi/captures/gen.cmd
@@ -1,3 +1,2 @@
-ffmpeg -i "rewrap.mp4" -r 20 -filter:v "setpts=0.5*PTS" -codec:v libx264 -profile:v baseline -pix_fmt yuv420p -movflags +faststart -crf 30 -preset veryslow fast.mp4
+ffmpeg -f image2  -i "2015-08-04_%%08d.png" -r 15 -filter:v "setpts=1*PTS" -codec:v libx264 -profile:v baseline -pix_fmt yuv420p -movflags +faststart -crf 30 -preset veryslow fast.mp4
 
--f image2  -i "2015-07-11_%%08d.png" -r 60
