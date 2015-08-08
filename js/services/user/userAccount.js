598d4749f00446820fdc9fa29341002ff82c8d11
PandaChauve <sylvain.chevremont@gmail.com>
2015-08-08 10:32:53 +0200

    tolowercase for old stored data

--- js/services/user/userAccount.js
+++ js/services/user/userAccount.js
@@ -126,8 +126,8 @@ angular.module('angularApp.factories')
             return this.username !== null;
         };
         User.prototype.loadTheme = function () {
-            var t = storage.get(storage.Keys.WebTheme) || "cyborg";
-            var s = localStorage.getItem("dynamic_css_name");
+            var t = storage.get(storage.Keys.WebTheme).toLowerCase() || "cyborg";
+            var s = localStorage.getItem("dynamic_css_name").toLowerCase();
             if (s != t) { //already good don't reload
                 api.getTheme(t).success(function(data){
                     localStorage.setItem("dynamic_css_name", t);
