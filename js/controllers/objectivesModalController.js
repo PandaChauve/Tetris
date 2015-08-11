ef30324d33a92187a6f311b9daa416ec7ba2818f
PandaChauve <sylvain.chevremont@gmail.com>
2015-08-11 18:51:10 +0200

    display campaign in popup

--- js/controllers/objectivesModalController.js
+++ js/controllers/objectivesModalController.js
@@ -1,10 +1,20 @@
 angular.module('angularApp.controllers')
-    .controller('ObjectivesModalCtrl', ['$scope', '$modalInstance', 'stateChecker', 'gameConfig',
-        function ($scope, $modalInstance, stateChecker, gameConfig) {
+    .controller('ObjectivesModalCtrl', ['$scope', '$modalInstance', 'stateChecker', 'gameName', 'gameConfig',
+        function ($scope, $modalInstance, stateChecker, gameName, gameConfig) {
             "use strict";
-           $scope.continue = function(){
-               $modalInstance.close();
-           };
-           $scope.rules = stateChecker.createRuleSet(gameConfig.victory);
-           $scope.tips = gameConfig.tooltip;
+            $scope.continue = function () {
+                $modalInstance.close();
+            };
+            $scope.rules = stateChecker.createRuleSet(gameConfig.victory);
+            $scope.tips = gameConfig.tooltip;
+            $scope.title = nameToTitle();
+            function nameToTitle(){
+                var n = gameName.split('/');
+                n = n[n.length-1];
+                n = n.replace("_", " ");
+                n = n.replace("_", " ");
+                n = n.replace("ta", "Tetris Attack - ");
+                n = n[0].toUpperCase() + n.slice(1)+"/10";
+                return n;
+            }
         }]);
